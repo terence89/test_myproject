e2992bf2226ab7e79d21500a490f4026d8e5a905
terence <terence9396@163.com>
2020-08-16 22:37:26 +0800

    提交接口脚本

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
-# -*- coding:utf-8 _*-
-""" 
-@author:mongo
-@time: 2018/12/17 
-@email:3126972006@qq.com
-@function： 
-"""
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/11 20:03
+# @Author  : lemon_huahua
+# @Email   : 204893985@qq.com
+# @File    : __init__.py.py
